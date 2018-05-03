4129d5a54853561b5ba208ac0203a64740be501a
Yudi Dwijayanto <yudidgreat@Yudis-MacBook-Pro.local>
2018-05-03 20:20:37 +0700

    Latihan Variable di JS

---
+++ variable.js
@@ -0,0 +1,40 @@
+// old version
+// var name = 'Yudi;'  untuk Java Script old version
+
+// new version
+// const city = 'jakarta' --> constant tidak dapat di ganti or tidak boleh di reassign
+// let phone = '08161618908' --> dapat di reassign untuk nilai variablenya
+// untuk check or runnya di terminal mac
+
+// const angka_satu = 8
+// const angka_dua = 2
+// const hasil
+
+// const hasil_tandingan = angka_satu + angka_dua
+// hasil = hasil_tandingan
+// console.log (hasil)
+
+// const harus ada variablenya tidak boleh kosong --- const hasil tidak ada nilainya
+// variable hasil yang menggunakan const di reassign dengan variable lain sehingga akan error, harusnya jika akan di reassign harus menggunakan let
+// keyword const vs let [lihat penjelasan di atas]
+
+// 2. Data type
+const name = "yudi"; // string data
+const age = 17; // integer
+const nomimal = 20.5; // double
+const ishandsome = true; // boolean -- hanya bisa true or false
+const cars = ["honda", "toyota", "mitsubishi"]; // array
+const angkas = [1, 2, 3, 4]; // array of number
+const info = {
+  address: "blablaba",
+  city: "blablaba,"
+}; // object
+// untuk data tipe objek bisa terdiri dari beberapa properties yang dimana 1 properties tersusun dari Key - value pairs
+
+// unutk menampilkan data 
+// console.log(nama variable yang ingin di tampilkan)
+//console.log (nama + ' ' + umur)
+//console.log (cars [1]) array always has index , record paling awal adalah 0
+//console.log (info.elementyang akna di tampilkan)
+//console.log (info.address +'' +info.city)
+//2018
